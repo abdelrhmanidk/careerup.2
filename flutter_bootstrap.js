0b4fb0867a2892b984ffa388820ad003e9b98697
abdelrhmanidk <abdelrhmanbdlhlm@gmail.com>
2024-07-05 16:29:19 +0300

    careerup.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4080777426"
+    serviceWorkerVersion: "1691305769"
   }
 });
